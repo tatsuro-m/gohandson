3d29202faab53f7a9bc62f1b4da16676fb56aa4c
Tatsuro Miyamoto <mtatsu0130@gmail.com>
2021-05-05 08:32:02 +0900

    complete

--- greeting/skeleton/step06/export_test.go
+++ greeting/skeleton/step06/export_test.go
@@ -1,15 +1,20 @@
 // STEP06: テストのパッケージを分けよう
 package greeting
 
-import "golang.org/x/text/language"
+import (
+	"fmt"
+
+	"golang.org/x/text/language"
+)
 
 // パッケージ変数langを一時的に変更する関数
 // greetingパッケージだがファイル名が_test.goで終わるため
 // go testの際しかビルドされない
 func ExportSetLang(l language.Tag) func() {
 	orgLang := lang
+	fmt.Println(orgLang)
 	lang = l
 	return func() {
-		// TODO: langを元に戻す
+		lang = orgLang
 	}
 }
